92b094f5779ffcb60d1d0ce65ec93c73c07238d7
fmealing <161133737+fmealing@users.noreply.github.com>
2024-05-25 13:14:42 +0100

    Update song.py

--- Python/Week1/song.py
+++ Python/Week1/song.py
@@ -8,7 +8,7 @@ class Song:
         duration (int): The duration of the song in seconds. May be zero
     """
 
-    def __init__(self, title, artist, duration):
+    def __init__(self, title, artist, duration=0):
         self.title = title
         self.artist = artist
         self.duration = duration
@@ -30,9 +30,9 @@ class Album:
     """ Class to represent a album, using its tracklist
 
     Attributes:
-        album_name (str): The name of the album
+        name (str): The name of the album
         year (int): The year the album was released
-        atrist (Artist): hew artist responsible to  the album.
+        artist (Artist): hew artist responsible to  the album.
             If not specified, the artist will default to an artist with the name
             "Various Artists".
         tracks (List[Song]): A list of the songs on the album.
@@ -45,7 +45,7 @@ class Album:
         self.name = name
         self.year = year
         if artist is None:
-            self.artist = artist("Various Artists")
+            self.artist = Artist("Various Artists")
         else:
             self.artist = artist
 
@@ -64,3 +64,90 @@ class Album:
             self.tracks.append(song)
         else:
             self.tracks.insert(position, song)
+
+
+class Artist:
+    """Basic class to store artist details.
+
+    Attributes:
+        name (str): The name of the artist
+        albums (list[album]): A list of the album by the artist.
+            The list includes only those albums in the collection, it is
+            not an exhaustive list of the artist's published albums.
+
+    Methods:
+        add_album: Use to add a new album to the artist's album list
+    """
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album):
+        """Add a new album to the list.
+
+        Arguments:
+            album (album): Album object to add to the list.
+                If the album is already present, it will not be added again (although this is yet to be implemented)
+        """
+        self.albums.append(album)
+
+
+def load_data():
+    new_artist = None
+    new_album = None
+    artist_list = []
+
+    with open("albums.txt", "r") as albums:
+        for line in albums:
+            # data row should consist of (artist, album, year, song)
+            artist_field, album_field, year_field, song_field = tuple(line.strip("\n").split("\t"))
+            print(f"{artist_field}: {album_field}: {year_field}: {song_field}")
+
+            if new_artist is None:
+                new_artist = Artist(artist_field)
+            elif new_artist.name != artist_field:
+                # just read details for a new artist
+                # store the current album in the current artists collection then create a new artist object
+                new_artist.add_album(new_album)
+                artist_list.append(new_artist)
+                new_artist = Artist(artist_field)
+                new_album = None
+
+            if new_album is None:
+                new_album = Album(album_field, year_field, new_artist)
+            elif new_album.name != album_field:
+                # just read a new album for a current artist
+                # store the current album in the artist's collection then create a new album object
+                new_artist.add_album(new_album)
+                new_album = Album(album_field, year_field, new_artist)
+
+            # create a new song object and add it to the current album's collection
+            new_song = Song(song_field, new_artist)
+            new_album.add_song(new_song)
+
+        # after reading the last line of the text file, we will have an artist and album that haven't
+        # been sored - process them now
+        if new_artist is not None:
+            if new_album is not None:
+                new_artist.add_album(new_album)
+            artist_list.append(new_artist)
+
+    return artist_list
+
+
+def create_checkfile(artist_list):
+    """Create a check file from the object data for comparison with the original file"""
+    with open("checkfile.txt", "w") as checkfile:
+        for new_artist in artist_list:
+            for new_album in new_artist.albums:
+                for new_song in new_album.tracks:
+                    print("{0.name}\t {1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song),
+                          file=checkfile)
+
+
+if __name__ == "__main__":
+    artists = load_data()
+    print("There are {} artists".format(len(artists)))
+
+    create_checkfile(artists)
+    
