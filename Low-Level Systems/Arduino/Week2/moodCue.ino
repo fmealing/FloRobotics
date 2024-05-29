976e2a309a8f7eb051042fde2667f5be91f2e359
fmealing <161133737+fmealing@users.noreply.github.com>
2024-05-29 15:56:39 +0100

    Create moodCue.ino

---
+++ Low-Level Systems/Arduino/Week2/moodCue.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo myServo;
+
+int const potPin = A0;
+int potVal;
+int angle;
+
+void setup() {
+  myServo.attach(9);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  potVal = analogRead(potPin);
+  Serial.print("poitVal: ");
+  Serial.print(potVal);
+
+  angle = map(potVal, 0, 123, 0, 179);
+  Serial.print(", angle: ");
+  Serial.println(angle);
+
+  myServo.write(angle);
+  delay(15);
+}
